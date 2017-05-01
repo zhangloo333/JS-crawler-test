59e6d11ad82906f84da86c08b8f6bde05173cd6e
ebookcode123 <ebookcode123@gmail.com>
2017-05-01 10:30:14 -0700

    add how to download page.

--- main.js
+++ main.js
@@ -2,18 +2,31 @@ var express = require('express');
 var path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
-// var fs = require('fs');
+var fs = require('fs');
 var app = express();
 var port = 8000;
 
-var url = 'https://www.google.com/';
-request(url, function(err, resp, body) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(body);
-    }
-});
+// var url = 'https://www.google.com/';
+// request(url, function(err, resp, body) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(body);
+//     }
+// });
 
+//     	//download html to file
+var url = 'https://www.google.com/';
+var destination = fs.createWriteStream('./download/google3.html');
+request(url).pipe(destination);
+// .on('finish',function(){
+// 	console.log('done');
+// })
+// .on('error',function(err){
+// 	console.log(err);
+// });
+destination.on('finish', function(){
+	console.log('all done');
+})
 app.listen(port);
 console.log('server is runing on ', port);
